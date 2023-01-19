2d4465f2132dfe92719362c8c2564d34779b7b80
ferdaws99 <ferdaws.laamouri@esprit.tn>
2023-01-19 11:43:16 +0000

    configuration de la langue de l'application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{registerLocaleData} from '@angular/common';
+import * as fr from '@angular/common/locales/fr'
 import { AppComponent } from './app.component';
 import { MyGalComponent } from './my-gal/my-gal.component';
 
@@ -12,7 +13,13 @@ import { MyGalComponent } from './my-gal/my-gal.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {provide:LOCALE_ID,useValue:'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(){
+    registerLocaleData(fr.default)
+  }
+}
